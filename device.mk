a6d371d51cdb95ced65e3da2c95753941a99e9f2
Jake Weinstein <jake@aospa.co>
2022-06-15 19:17:44 +0100

    pdx225: Use speed tuning for performance critical applications
    The default ART setting is quicken: run DEX code verification and
    optimize some DEX instructions to get better interpreter performance.
    
    This commit switches performance critical applications to speed:
    run DEX code verification and AOT-compile all methods.
    
    PRODUCT_DEXPREOPT_SPEED_APPS (New in Android O)
    List of applications that have been identified as core to the products
    and which are desirable to compile with the speed compiler filter.  For
    example, persistent apps such as SystemUI get a chance to use
    profile-guided compilation only at the next reboot, so it may be better
    for the product to have these apps always AOT-compiled.

--- device.mk
+++ device.mk
@@ -548,6 +548,10 @@ PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 WITH_DEXPREOPT_DEBUG_INFO := false
 
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    Settings \
+    SystemUI
+
 # Touch
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.sony
