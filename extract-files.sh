548aa82339ad9cd1d3daed8177536920d8c46c95
Lucchetto <lucchetto.tie@live.com>
2022-09-03 13:59:21 +0530

    pdx225: Patch egistec interface for loading older libhidlbase.so
    _ZN7android8hardware7details17gBnConstructorMapE symbol have been removed on Android 13
    
    Change-Id: I168cb40a55c045643889a00b34d2d3893306cddb

--- extract-files.sh
+++ extract-files.sh
@@ -61,6 +61,10 @@ function blob_fixup() {
     vendor/lib64/libssc.so | vendor/lib64/libsnsapi.so | vendor/lib64/libsensorcal.so | vendor/lib64/libsnsdiaglog.so | vendor/lib64/sensors.ssc.so | vendor/bin/sensors.qti | vendor/lib64/libwvhidl.so | vendor/lib/libssc.so )
         "${PATCHELF} --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
         ;;
+        vendor/lib64/vendor.egistec.hardware.biometrics.fingerprint@4.0.so)
+            "${PATCHELF_0_8}" --remove-needed "libhidlbase.so" "${2}"
+            --add-needed "libhidlbase-v32.so" "${2}"
+            ;;
     esac
 }
 
