47a315cba79d4acc995342a65aa7a718a75dbab9
NextWorks123 <nextdesigns@libero.it>
2023-03-13 16:45:35 +0100

    pdx225: Drop QTI Thermal Hal
    - Better to clean as we it doesn't work on pdx225
    
    - Entirely not working:
    I ThermalHalWrapper: Thermal HAL 2.0 service connected.
    E ThermalHalWrapper: Couldn't get temperatures because of HAL error: ThermalHAL not initialized properly.
    W ThermalManagerService: Thermal HAL reported invalid data, abort connection
    E ThermalHalWrapper: Couldn't get temperature thresholds because of HAL error: ThermalHAL not initialized properly.

--- device.mk
+++ device.mk
@@ -520,13 +520,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
-
 # Touch
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.sony
