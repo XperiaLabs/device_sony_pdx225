79d65a9306bf36a0b72587e5a903290ae2dcde77
John Galt <johngaltfirstrun@gmail.com>
2023-08-31 10:46:52 +0300

    pdx225: Make dex2oat go fast Make ota updater go a bit faster.
    Change-Id: I3159a0d16d8ce9c6fcb3a138384f2e2f47200a57
    Signed-off-by: JaswalAshish <ashish@m.ms.evolution-x.org>

--- device.mk
+++ device.mk
@@ -273,6 +273,13 @@ PRODUCT_PACKAGES += \
     init.sony-platform.rc \
     init.sony.rc
 
+# Dex to make OTA go faster
+PRODUCT_VENDOR_PROPERTIES += \
+    dalvik.vm.boot-dex2oat-cpu-set=1,2,3,4,5,6,7 \
+    dalvik.vm.boot-dex2oat-threads=7 \
+    dalvik.vm.image-dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+    dalvik.vm.image-dex2oat-threads=8
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
