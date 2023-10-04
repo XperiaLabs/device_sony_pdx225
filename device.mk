953197a4d5327e6d661d452349570fa428042c6f
Sarthak Roy <sarthakroy2002@gmail.com>
2023-10-04 20:36:50 +0530

    pdx225: Switch to Wifi service AIDL
    Change-Id: I5a2864b4159b7815eff4dd2f0de02a3dabbacfa5
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- device.mk
+++ device.mk
@@ -546,7 +546,7 @@ PRODUCT_PACKAGES += \
 
 # WiFi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     hostapd \
     libwpa_client \
     libwifi-hal-ctrl \
