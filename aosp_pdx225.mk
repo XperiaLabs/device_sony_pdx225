92eecf9c2ea45a1511ab33e72fa3f339ec53d80a
Mohammad Sayful Islam <Sayf.mohammad01@gmail.com>
2023-10-31 18:54:41 +0000

    pdx225: bring upto goolag

--- aosp_pdx225.mk
+++ aosp_pdx225.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 IS_PHONE := true
 
